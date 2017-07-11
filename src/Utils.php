70a0961fade70aafae0f374202e5c3dcad3a018e
Administrator <admin@example.com>
2017-07-11 15:40:16 +0400

    class, composer, rename, foldr

---
+++ src/Utils.php
@@ -0,0 +1,249 @@
+<?php
+
+namespace LWB\Functional;
+
+class Utils
+{
+
+	/**
+	 * Functional functions for PHP.
+	 *
+	 * Please see accompanying README for more information.
+	 *
+	 * @copyright Kim Burgestrand
+	 * @license X11 license (MIT license)
+	 *          @homepage http://github.com/Burgestrand/Funcy
+	 */
+	
+	/**
+	 * Function composition
+	 *
+	 * Example (pseudocode)
+	 * --------------------
+	 * fa(x): return x + "a"
+	 * fb(y): return y + "b"
+	 * fc(z): return z + "c"
+	 *
+	 * print sequence(fa, fb, fc)('❤')
+	 * // prints: ❤cba
+	 *
+	 * GOTCHA: Errors triggered by non-existing functions will have their
+	 * index in reverse order: `arg#0` is actually the last argument. This
+	 * is due to the fact that `compose` is defined using `sequence`.
+	 *
+	 * @param
+	 *        	callback₁, callback₂, … callbackᵪ
+	 * @return closure ∫(callback₁(callback₂(callbackᵪ(…))))
+	 */
+	public static function compose()
+	{
+		$fns = func_get_args();
+		return call_user_func_array([
+				__CLASS__,
+				'sequence' 
+		], array_reverse($fns));
+	}
+
+	/**
+	 * Function composition in sequence
+	 *
+	 * Example (pseudocode)
+	 * --------------------
+	 * fa(x): return x + "a"
+	 * fb(y): return y + "b"
+	 * fc(z): return z + "c"
+	 *
+	 * print sequence(fa, fb, fc)('❤')
+	 * // prints: ❤abc
+	 *
+	 * @param
+	 *        	callback₁, callback₂, … callbackᵪ
+	 * @return closure ∫(callbackᵪ(callback₂(callback₁(…))))
+	 */
+	public static function sequence()
+	{
+		$fns = func_get_args();
+		
+		foreach ($fns as $i => $fn)
+		{
+			if (!is_callable($fn, FALSE, $name))
+			{
+				trigger_error("All arguments must be valid callbacks (arg#{$i}: “${name}” failed)", E_USER_ERROR);
+			}
+		}
+		
+		return function () use ($fns)
+		{
+			$args = func_get_args();
+			$acc = call_user_func_array(array_shift($fns), $args);
+			
+			foreach ($fns as $fn)
+			{
+				$acc = $fn($acc);
+			}
+			
+			return $acc;
+		};
+	}
+
+	/**
+	 * Partial function application
+	 *
+	 * Example (pseudocode)
+	 * --------------------
+	 * fa(greet, name): return greet + ' ' + name
+	 * fx = curry(fa, 'Hello')
+	 *
+	 * fx('Kim') // Hello Kim
+	 * fx('Elin') // Hello Elin
+	 *
+	 * @param
+	 *        	callback
+	 * @param
+	 *        	a1, a2…
+	 * @return closure ∫(callback(a1, a2, …))
+	 */
+	public static function partial($fn)
+	{
+		$args = func_get_args();
+		array_shift($args);
+		
+		if (!is_callable($fn))
+		{
+			trigger_error('First argument must be a valid callback', E_USER_ERROR);
+		}
+		
+		return function () use ($fn, $args)
+		{
+			$xargs = func_get_args();
+			return call_user_func_array($fn, array_merge($args, $xargs));
+		};
+	}
+
+	/**
+	 * Execute the callback on each element in the sequence
+	 *
+	 * Example (pseudocode)
+	 * --------------------
+	 * greet(name): return uppercase(name)
+	 * names = 'Kim', 'Elin', 'Kelin'
+	 *
+	 * map(greet, names) // 'KIM', 'ELIN', 'KELIN'
+	 *
+	 * @see array_map
+	 * @param
+	 *        	callback
+	 * @param
+	 *        	Iterator
+	 * @return array
+	 */
+	public static function map($fn, $iterable)
+	{
+		if (!is_callable($fn))
+		{
+			trigger_error('First argument must be a valid callback', E_USER_ERROR);
+		}
+		
+		$result = array();
+		
+		foreach ($iterable as $key => $val)
+		{
+			$result[$key] = $fn($val);
+		}
+		
+		return $result;
+	}
+
+	/**
+	 * Executes the callback on `init` and the first element, then it executes
+	 * the callback on the result of the previous execution and the second element
+	 * and so on.
+	 *
+	 * Example (pseudocode)
+	 * --------------------
+	 * add(a, b): return a + b
+	 * numbers = 0, 1, 2, 3, 4, 5
+	 *
+	 * print reduce(add, 0, numbers) // 15
+	 *
+	 * @param
+	 *        	callback
+	 * @param
+	 *        	mixed init
+	 * @param
+	 *        	Iterator
+	 * @return array
+	 */
+	public static function foldl($fn, $init, $iterable)
+	{
+		if (!is_callable($fn))
+		{
+			trigger_error('First argument must be a valid callback', E_USER_ERROR);
+		}
+		
+		$acc = $init;
+		
+		foreach ($iterable as $x)
+		{
+			$acc = $fn($acc, $x);
+		}
+		
+		return $acc;
+	}
+
+	public static function iter_reverse($iterable)
+	{
+		for (end($iterable); ($key = key($iterable)) !== null; prev($iterable))
+		{
+			yield $key => current($iterable);
+		}
+	}
+
+	public static function foldr($fn, $init, $iterable)
+	{
+		if (!is_callable($fn))
+		{
+			trigger_error('First argument must be a valid callback', E_USER_ERROR);
+		}
+		
+		$acc = $init;
+		
+		foreach (self::iter_reverse($iterable) as $x)
+		{
+			$acc = $fn($acc, $x);
+		}
+		
+		return $acc;
+	}
+
+	/**
+	 * Flips the (first two) arguments of a function.
+	 *
+	 * Example (pseudocode)
+	 * --------------------
+	 * pow(a, b): return a ** b
+	 * wop: flip(pow)
+	 *
+	 * print wop(10, 2) // 1024
+	 *
+	 * @param
+	 *        	callback
+	 * @return closure
+	 */
+	public static function flip($fn)
+	{
+		return function () use ($fn)
+		{
+			$args = func_get_args();
+			$a = array_shift($args);
+			$b = array_shift($args);
+			$args = array_merge(array(
+					$b,
+					$a 
+			), $args);
+			return call_user_func_array($fn, $args);
+		};
+	}
+	
+	/* End of file funcy.php */
+}
--- tests/funcy.php
+++ tests/funcy.php
@@ -1,7 +1,9 @@
 <?php
-  require '../lib/funcy.php';
+  require '../src/Utils.php';
   require './asserts.php';
   
+  use LWB\Functional\Utils as FN;
+
   // Helper functions
   function println($str)
   {
@@ -14,7 +16,7 @@
     $a = function($x) { return $x . 'a'; };
     $b = function($x) { return $x . 'b'; };
     $c = function($x) { return $x . 'c'; };
-    $fn = compose($a, $b, $c);
+    $fn = FN::compose($a, $b, $c);
     
     return is_identical($fn('d'), 'dcba');
   }
@@ -24,7 +26,7 @@
     $a = function($x) { return $x . 'a'; };
     $b = function($x) { return $x . 'b'; };
     $c = function($x) { return $x . 'c'; };
-    $fn = sequence($a, $b, $c);
+    $fn = FN::sequence($a, $b, $c);
     
     return is_identical($fn('d'), 'dabc');
   }
@@ -32,7 +34,7 @@
   function test_curry()
   {
     $a = function($a, $b) { return $a + $b; };
-    $add5 = curry($a, 5);
+    $add5 = FN::partial($a, 5);
     
     return is_identical($add5(10), 15);
   }
@@ -42,15 +44,23 @@
     $a = function($x) { return $x * $x; };
     $range = range(0, 10);
     
-    return is_identical(map($a, $range), array_map($a, $range));
+    return is_identical(FN::map($a, $range), array_map($a, $range));
   }
   
   function test_reduce()
   {
-    $a = function($acc, $x) { return $acc + $x; };
-    $range = range(0, 5);
+    $a = function($acc, $x) { return $x ** $acc; };
+    $range = range(1, 4);
     
-    return is_identical(reduce($a, 1, $range), 16); // 1+0+1+2+3+4+5
+    return is_identical(FN::foldl($a, 0, $range), 262144); // 4 ** 3 ** 2 ** 1 Exponential factorial
+  }
+
+  function test_reduce_right()
+  {
+  	$a = function($acc, $x) { return $x ** $acc; };
+  	$range = range(3, 4);
+  	
+  	return is_identical(FN::foldr($a, 1, $range), 81); // 3 ** 4 ** 1 (pow is right-associative)
   }
   
   function test_flip()
@@ -58,8 +68,8 @@
     $a = function($a, $b) { return pow($a, $b); };
     $b = function($a, $b, $c) { return $a . $b . $c; };
     
-    $af = flip($a);
-    $bf = flip($b);
+    $af = FN::flip($a);
+    $bf = FN::flip($b);
     
     return is_identical($af(10, 2), $a(2, 10))
         && is_identical($bf('a', 'b', 'c'), $b('b', 'a', 'c'));
